faf5a416732ad1adb7fd3ad02a16b9bea1c8272b
Andrew Dennis <adennis4@gmail.com>
2012-03-05 16:46:20 -0800

    User signup NewT witter complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,21 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
     @title = "Sign up"
   end
 
   def full_title
   end
   
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the newT witter!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
